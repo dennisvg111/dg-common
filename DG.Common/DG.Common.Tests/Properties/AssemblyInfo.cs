5d379b8e64b46a26bd63f1b7d09c5840887d2493
dennisvg111 <dennisvg111@gmail.com>
2023-04-25 16:15:29 +0200

    Add test project

---
+++ DG.Common/DG.Common.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("07d6983b-d4ac-4e44-a401-815406988b24")]
