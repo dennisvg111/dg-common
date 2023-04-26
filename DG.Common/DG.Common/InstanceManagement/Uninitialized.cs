c5c430be8a2032290f4e9ecbf0f5004db732afb8
dennisvg111 <dennisvg111@gmail.com>
2023-04-26 17:39:43 +0200

    Add uninitialized class

---
+++ DG.Common/DG.Common/InstanceManagement/Uninitialized.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Runtime.Serialization;
+
+namespace DG.Common.InstanceManagement
+{
+    /// <summary>
+    /// Provides functionality to create uninitialized instances of a given type.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public static class Uninitialized<T>
+    {
+        private static readonly T _instance;
+
+        /// <summary>
+        /// Returns an unitialized instance of the given type <typeparamref name="T"/>. Note that this always returns the same instance.
+        /// </summary>
+        public static T Instance => _instance;
+
+        static Uninitialized()
+        {
+            _instance = New();
+        }
+
+        /// <summary>
+        /// Returns a new unitialized instance of the given type <typeparamref name="T"/>.
+        /// </summary>
+        /// <returns></returns>
+        public static T New()
+        {
+            Type t = typeof(T);
+            if (HasDefaultConstructor(t))
+            {
+                var constructor = Expression.Lambda<Func<T>>(Expression.New(t)).Compile();
+                return constructor();
+            }
+
+            return (T)FormatterServices.GetUninitializedObject(t);
+        }
+
+        private static bool HasDefaultConstructor(Type t)
+        {
+            return t.IsValueType || t.GetConstructor(Type.EmptyTypes) != null;
+        }
+    }
+}
