9f984262818e2bde88d6589434d90029703a0cde
dennisvg111 <dennisvg111@gmail.com>
2023-04-28 15:32:03 +0200

    Add Throws.If exception helpers (#7)

---
+++ DG.Common/DG.Common.Tests/XUnitHelpers/MoreAsserts.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Xunit;
+
+namespace DG.Common.Tests.XUnitHelpers
+{
+    public static class MoreAsserts
+    {
+        public static void NoExceptions(Action testCode)
+        {
+            var exception = Record.Exception(testCode);
+            Assert.Null(exception);
+        }
+    }
+}
---
+++ DG.Common/DG.Common/Exceptions/Throws.cs
@@ -0,0 +1,18 @@
+﻿namespace DG.Common.Exceptions
+{
+    /// <summary>
+    /// Provides methods that throw exceptions on specific conditions, to reduce boilerplate code.
+    /// </summary>
+    public class Throws
+    {
+        private Throws()
+        {
+
+        }
+
+        /// <summary>
+        /// The entry point for the <see cref="Throws"/> class.
+        /// </summary>
+        public static Throws If => new Throws();
+    }
+}
