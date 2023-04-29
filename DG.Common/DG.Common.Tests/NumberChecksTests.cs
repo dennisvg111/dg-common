d678597e4622f68ba8141916b973081e646f7979
dennisvg111 <dennisvg111@gmail.com>
2023-04-29 16:54:36 +0200

    Add number checks, rename Enumerable to Collection (#10)

---
+++ DG.Common/DG.Common.Tests/NumberChecksTests.cs
@@ -0,0 +1,109 @@
+﻿using DG.Common.Exceptions;
+using DG.Common.Tests.XUnitHelpers;
+using System;
+using Xunit;
+
+namespace DG.Common.Tests
+{
+    public class NumberChecksTests
+    {
+        [Theory]
+        [InlineData(0)]
+        public void IntIsZero_ShouldThrow(int value)
+        {
+            Action check = () => ThrowIf.Number(value, nameof(value)).IsZero();
+
+            Assert.Throws<ArgumentException>(check);
+        }
+
+        [Theory]
+        [InlineData(0)]
+        public void ByteIsZero_ShouldThrow(byte value)
+        {
+            Action check = () => ThrowIf.Number(value, nameof(value)).IsZero();
+
+            Assert.Throws<ArgumentException>(check);
+        }
+
+        [Theory]
+        [InlineData(0)]
+        public void DoubleIsZero_ShouldThrow(double value)
+        {
+            Action check = () => ThrowIf.Number(value, nameof(value)).IsZero();
+
+            Assert.Throws<ArgumentException>(check);
+        }
+
+        [Theory]
+        [InlineData(-1)]
+        [InlineData(1)]
+        public void IntIsZero_ShouldNotThrow(int value)
+        {
+            Action check = () => ThrowIf.Number(value, nameof(value)).IsZero();
+
+            MoreAsserts.NoExceptions(check);
+        }
+
+        [Theory]
+        [InlineData(-0.0001)]
+        [InlineData(0.0001)]
+        public void DoubleIsZero_ShouldNotThrow(double value)
+        {
+            Action check = () => ThrowIf.Number(value, nameof(value)).IsZero();
+
+            MoreAsserts.NoExceptions(check);
+        }
+
+        [Theory]
+        [InlineData(-1)]
+        public void IsNegative_ShouldThrow(int value)
+        {
+            Action check = () => ThrowIf.Number(value, nameof(value)).IsNegative();
+
+            Assert.Throws<ArgumentException>(check);
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(1)]
+        public void IntIsNegative_ShouldNotThrow(int value)
+        {
+            Action check = () => ThrowIf.Number(value, nameof(value)).IsNegative();
+
+            MoreAsserts.NoExceptions(check);
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(1)]
+        public void ByteIsNegative_ShouldNotThrow(byte value)
+        {
+            Action check = () => ThrowIf.Number(value, nameof(value)).IsNegative();
+
+            MoreAsserts.NoExceptions(check);
+        }
+
+        [Theory]
+        [InlineData(0.9, 1.0, 10.0)]
+        [InlineData(10.1, 1.0, 10.0)]
+        [InlineData(-0.9, -1.0, -10.0)]
+        [InlineData(-10.1, -1.0, -10.0)]
+        public void IsNotBetweenInclusive_ShouldThrow(double value, double min, double max)
+        {
+            Action check = () => ThrowIf.Number(value, nameof(value)).IsNotBetweenInclusive(min, max);
+
+            Assert.Throws<ArgumentException>(check);
+        }
+
+        [Theory]
+        [InlineData(1.0, 1.0, 10.0)]
+        [InlineData(10.0, 1.0, 10.0)]
+        [InlineData(0, -10.0, 10.0)]
+        public void IsNotBetweenInclusive_ShouldNotThrow(double value, double min, double max)
+        {
+            Action check = () => ThrowIf.Number(value, nameof(value)).IsNotBetweenInclusive(min, max);
+
+            MoreAsserts.NoExceptions(check);
+        }
+    }
+}
---
+++ DG.Common/DG.Common/GenericNumbers/Operators.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace DG.Common.GenericNumbers
+{
+    public readonly struct Operators<T> : IEquatable<Operators<T>> where T : struct, IComparable, IComparable<T>, IConvertible, IEquatable<T>, IFormattable
+    {
+        private readonly T _value;
+
+        public Operators(T value)
+        {
+            _value = value;
+        }
+
+        public static implicit operator Operators<T>(T value)
+        {
+            return new Operators<T>(value);
+        }
+
+        public static implicit operator T(Operators<T> value)
+        {
+            return value._value;
+        }
+
+        public static bool operator ==(Operators<T> left, Operators<T> right)
+        {
+            return (left.Equals(right));
+        }
+
+        public static bool operator !=(Operators<T> left, Operators<T> right)
+        {
+            return !(left == right);
+        }
+
+        public static bool operator >(Operators<T> left, Operators<T> right)
+        {
+            return left._value.CompareTo(right._value) > 0;
+        }
+
+        public static bool operator <(Operators<T> left, Operators<T> right)
+        {
+            return left._value.CompareTo(right._value) < 0;
+        }
+
+        public static bool operator >=(Operators<T> left, Operators<T> right)
+        {
+            return left._value.CompareTo(right._value) >= 0;
+        }
+
+        public static bool operator <=(Operators<T> left, Operators<T> right)
+        {
+            return left._value.CompareTo(right._value) <= 0;
+        }
+
+        /// <inheritdoc/>
+        public bool Equals(Operators<T> other)
+        {
+            return _value.Equals(other._value);
+        }
+
+        /// <inheritdoc/>
+        public override bool Equals(object obj)
+        {
+            if ((obj is Operators<T>))
+            {
+                return Equals((Operators<T>)obj);
+            }
+            return _value.Equals(obj);
+        }
+
+        /// <inheritdoc/>
+        public override int GetHashCode()
+        {
+            return HashCode.Of(_value);
+        }
+
+        /// <summary>
+        /// Returns the value that represents zero for type <typeparamref name="T"/>.
+        /// </summary>
+        public static Operators<T> Zero => _zero.Value;
+
+        private static readonly Lazy<Operators<T>> _zero = new Lazy<Operators<T>>(() => new Operators<T>(GenerateZero()));
+        private static T GenerateZero()
+        {
+            try
+            {
+                return (T)Convert.ChangeType(0, typeof(T));
+            }
+            catch (Exception)
+            {
+                return default(T);
+            }
+        }
+    }
+}
