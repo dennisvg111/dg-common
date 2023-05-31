4d13cb8c505e095a86e206b5418ef358be7d3e30
dennisvg111 <dennisvg111@gmail.com>
2023-06-01 00:16:16 +0200

    Update documentation

--- DG.Common/DG.Common/Threading/TaskLock.cs
+++ DG.Common/DG.Common/Threading/TaskLock.cs
@@ -11,6 +11,11 @@ namespace DG.Common.Threading
     {
         private readonly SemaphoreSlim _semaphore = new SemaphoreSlim(1, 1);
 
+        /// <summary>
+        /// Executes the given <see cref="Task"/> on only a single thread at a time.
+        /// </summary>
+        /// <param name="worker"></param>
+        /// <returns></returns>
         public async Task LockAsync(Func<Task> worker)
         {
             await LockAsync<int>(async () =>
@@ -20,10 +25,16 @@ namespace DG.Common.Threading
             });
         }
 
-        // overloading variant for non-void methods with return type (generic T)
+        /// <summary>
+        /// Executes the given <see cref="Task{T}"/> on only a single thread at a time, and returns the result.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="worker"></param>
+        /// <returns></returns>
         public async Task<T> LockAsync<T>(Func<Task<T>> worker)
         {
             var isTaken = false;
+
             try
             {
                 do
@@ -33,10 +44,13 @@ namespace DG.Common.Threading
                     }
                     finally
                     {
+                        //we use finally because it cannot be interrupted by ThreadAbortException, so isTaken is always correctly updated.
+                        //we interrupt the WaitAsync periodically so the calling thread does not get infinitely locked.
                         isTaken = await _semaphore.WaitAsync(TimeSpan.FromSeconds(1));
                     }
                 }
                 while (!isTaken);
+
                 return await worker();
             }
             finally
