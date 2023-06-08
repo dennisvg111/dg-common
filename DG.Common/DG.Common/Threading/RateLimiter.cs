10b6fe1edd411b5d86a8ae42e15e5ada68e5776b
dennisvg111 <dennisvg111@gmail.com>
2023-06-08 10:41:57 +0200

    Clean up rate limiter tests variable names (#22)
    * Clean up rate limiter tests variable names
    
    * Add documentation to RateLimiter.cs
    
    * Show better fail message in tests
    
    * Add total rate limit range to error message
    
    * Remove unneeded variable

--- DG.Common/DG.Common/Threading/RateLimiter.cs
+++ DG.Common/DG.Common/Threading/RateLimiter.cs
@@ -5,19 +5,31 @@ using System.Threading.Tasks;
 
 namespace DG.Common.Threading
 {
+    /// <summary>
+    /// A class that limits the number of tasks that can be executed asynchronously during a specific interval.
+    /// </summary>
     public class RateLimiter
     {
-        private readonly int _maxRequestsPerInterval;
         private readonly TimeSpan _interval;
         private readonly SemaphoreSlim _semaphore;
 
+        /// <summary>
+        /// Creates a new instance of <see cref="RateLimiter"/> with a given <paramref name="interval"/>, and an amount <paramref name="maxRequestsPerInterval"/> that can be executed during that interval.
+        /// </summary>
+        /// <param name="maxRequestsPerInterval"></param>
+        /// <param name="interval"></param>
         public RateLimiter(int maxRequestsPerInterval, TimeSpan interval)
         {
             _semaphore = new SemaphoreSlim(maxRequestsPerInterval);
-            _maxRequestsPerInterval = maxRequestsPerInterval;
             _interval = interval;
         }
 
+        /// <summary>
+        /// Executes the given task when allowed according to this <see cref="RateLimiter"/>, and returns the result.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="task"></param>
+        /// <returns></returns>
         public async Task<T> WaitFor<T>(Func<Task<T>> task)
         {
             await _semaphore.WaitAsync();
