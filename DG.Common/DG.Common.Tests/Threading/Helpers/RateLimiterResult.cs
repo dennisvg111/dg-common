622913093f2fbe3c5250566ea2d72f86390cd0a2
dennisvg111 <dennisvg111@gmail.com>
2023-06-08 15:25:03 +0200

    Add configure await (#23)
    * Rename WaitFor according to standards, add ConfigureAwait
    
    * Add RateLimiter ExecuteAsync overloads, refactor tests

---
+++ DG.Common/DG.Common.Tests/Threading/Helpers/RateLimiterResult.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace DG.Common.Tests.Threading.Helpers
+{
+    public class RateLimiterResult
+    {
+        private readonly DateTime _taskStartTime = DateTime.Now;
+        private DateTime _actualStartTime;
+
+        public DateTime TaskStartTime => _taskStartTime;
+        public TimeSpan RateLimitedFor => _actualStartTime - _taskStartTime;
+
+        public async Task SetActualStartTime()
+        {
+            _actualStartTime = DateTime.Now;
+
+            //wait so we can test if the rate limiter takes task time into account.
+            await Task.Delay(1000);
+        }
+
+        public override string ToString()
+        {
+            return RateLimitedFor.ToString();
+        }
+    }
+}
--- DG.Common/DG.Common/Threading/RateLimiter.cs
+++ DG.Common/DG.Common/Threading/RateLimiter.cs
@@ -30,26 +30,66 @@ namespace DG.Common.Threading
         /// <typeparam name="T"></typeparam>
         /// <param name="task"></param>
         /// <returns></returns>
-        public async Task<T> WaitFor<T>(Func<Task<T>> task)
+        public async Task<T> ExecuteAsync<T>(Func<Task<T>> task)
         {
-            await _semaphore.WaitAsync();
+            await _semaphore.WaitAsync().ConfigureAwait(false);
             var stopwatch = Stopwatch.StartNew();
             try
             {
-                return await task();
+                return await task().ConfigureAwait(false);
             }
             finally
             {
                 var remaining = _interval - stopwatch.Elapsed;
-                _ = ReleaseSemaphoreAfterDelayAsync(remaining);
+                ReleaseSemaphoreAfterDelayAsync(remaining);
             }
         }
 
-        private async Task ReleaseSemaphoreAfterDelayAsync(TimeSpan remaining)
+        /// <summary>
+        /// Executes the given task when allowed according to this <see cref="RateLimiter"/>.
+        /// </summary>
+        /// <param name="task"></param>
+        /// <returns></returns>
+        public async Task ExecuteAsync(Func<Task> task)
+        {
+            Func<Task<int>> function = async () =>
+            {
+                await task();
+                return 1;
+            };
+            await ExecuteAsync(function);
+        }
+
+        /// <summary>
+        /// Executes the given task when allowed according to this <see cref="RateLimiter"/>.
+        /// </summary>
+        /// <param name="task"></param>
+        /// <returns></returns>
+        public async Task ExecuteAsync(Action task)
+        {
+            Func<Task<int>> function = async () =>
+            {
+                await Task.Run(task);
+                return 1;
+            };
+            await ExecuteAsync(function);
+        }
+
+        /// <summary>
+        /// Executes the given task when allowed according to this <see cref="RateLimiter"/>, and returns the result.
+        /// </summary>
+        /// <param name="task"></param>
+        /// <returns></returns>
+        public async Task<T> ExecuteAsync<T>(Func<T> task)
+        {
+            return await ExecuteAsync(() => Task.Run(task));
+        }
+
+        private async void ReleaseSemaphoreAfterDelayAsync(TimeSpan remaining)
         {
             if (remaining.TotalMilliseconds > 0)
             {
-                await Task.Delay(remaining);
+                await Task.Delay(remaining).ConfigureAwait(false);
             }
             _semaphore.Release();
         }
