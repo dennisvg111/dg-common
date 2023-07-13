3ed0f2a51cc1ac4db8ecd465bac7d95aa0adec0f
dennisvg111 <dennisvg111@gmail.com>
2023-07-13 12:03:10 +0200

    Add Task extension methods

---
+++ DG.Common/DG.Common/Threading/TaskExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+
+namespace DG.Common.Threading
+{
+    /// <summary>
+    /// Provides extension methods for <see cref="Task{TResult}"/>.
+    /// </summary>
+    public static class TaskExtensions
+    {
+        /// <summary>
+        /// <para>Gets the result value of this <see cref="Task{TResult}"/>, unwrapping exceptions if needed.</para>
+        /// <para>Note that this is functionally the same as retrieving the result of <see cref="Task.GetAwaiter()"/></para>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="task"></param>
+        /// <returns>The result value of this <see cref="Task{TResult}"/>, which is the same as the task's type parameter.</returns>
+        public static T GetUnwrappedResult<T>(this Task<T> task)
+        {
+            return task.GetAwaiter().GetResult();
+        }
+    }
+}
