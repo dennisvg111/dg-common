c1af720ce1cb0fd6a42d88071725c3e5018d302b
dennisvg111 <dennisvg111@gmail.com>
2023-11-10 17:35:21 +0100

    Add SafeSync helper class (#30)
    * Add SafeSync helper class
    
    * Update version to 1.12.0

---
+++ src/Threading/SafeSync.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DG.Common.Threading
+{
+    /// <summary>
+    /// Provides functionality for safely calling asynchronous functions from an environment that doesn't support running code asynchronously.
+    /// </summary>
+    public static class SafeSync
+    {
+        private static readonly TaskFactory _myTaskFactory = new TaskFactory(CancellationToken.None, TaskCreationOptions.None, TaskContinuationOptions.None, TaskScheduler.Default);
+
+        /// <summary>
+        /// Runs the given asynchronous <paramref name="func"/> synchronously, and immediately returns the <typeparamref name="TResult"/> result.
+        /// </summary>
+        /// <typeparam name="TResult"></typeparam>
+        /// <param name="func"></param>
+        /// <returns></returns>
+        public static TResult Run<TResult>(Func<Task<TResult>> func)
+        {
+            return _myTaskFactory.StartNew(func).Unwrap().GetAwaiter().GetResult();
+        }
+
+        /// <summary>
+        /// Runs the given asynchronous <paramref name="func"/> synchronously.
+        /// </summary>
+        /// <param name="func"></param>
+        public static void Run(Func<Task> func)
+        {
+            _myTaskFactory.StartNew(func).Unwrap().GetAwaiter().GetResult();
+        }
+    }
+}
